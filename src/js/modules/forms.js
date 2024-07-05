ca59470fdacdd3851b237cf9fc744bf71d69f62e
Radu Bondarenco <radu.bondarenco91@gmail.com>
2024-07-05 21:02:10 +0100

    add forms

---
+++ src/js/modules/forms.js
@@ -0,0 +1,53 @@
+const forms = () => {
+	const form   = document.querySelectorAll('form');
+	const inputs = document.querySelectorAll('input');
+
+	const message = {
+		loading: 'Loading...',
+		success: 'You will be contacted soon',
+		failure: 'Something went wrong'
+	};
+
+	const postData = async (url, data) => {
+		document.querySelector('.status').textContent = message.loading;
+		let res = await fetch(url, {
+			method: 'Post',
+			body: data
+		});
+
+		return await res.text();
+	};
+
+	const clearInputs = () => {
+		inputs.forEach(item => {
+			item.value = '';
+		});
+	};
+
+	form.forEach(item => {
+		item.addEventListener('submit', e => {
+			e.preventDefault();
+
+			let statusMessage = document.createElement('div');
+			statusMessage.classList.add('status');
+			item.appendChild(statusMessage);
+
+			const formData = new FormData(item);
+
+			postData('assets/server.php', formData)
+				.then(res => {
+					console.log(res);
+					statusMessage.textContent = message.success;
+				})
+				.catch(() => (statusMessage.textContent = message.failure))
+				.finally(() => {
+					clearInputs();
+					setTimeout(() => {
+						statusMessage.remove();
+					}, 5000);
+				});
+		});
+	});
+};
+
+export default forms;
