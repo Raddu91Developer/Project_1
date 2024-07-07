b757a08eb2397634ce1220a58860331a5f968bd4
Radu Bondarenco <radu.bondarenco91@gmail.com>
2024-07-07 15:13:45 +0100

    add calculator

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,8 +1,8 @@
 const modals = () => {
 	function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
 		const trigger = document.querySelectorAll(triggerSelector);
-		const modal = document.querySelector(modalSelector);
-		const close = document.querySelector(closeSelector);
+		const modal   = document.querySelector(modalSelector);
+		const close   = document.querySelector(closeSelector);
 		const windows = document.querySelectorAll('[data-modal]');
 
 		trigger.forEach(item => {
@@ -32,10 +32,10 @@ const modals = () => {
 		});
 
 		modal.addEventListener('click', e => {
-      if (e.target === modal && closeClickOverlay) {
-         windows.forEach(item => {
-						item.style.display = 'none';
-					});
+			if (e.target === modal && closeClickOverlay) {
+				windows.forEach(item => {
+					item.style.display = 'none';
+				});
 
 				modal.style.display = 'none';
 				document.body.style.overflow = '';
@@ -58,8 +58,8 @@ const modals = () => {
 	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
 	bindModal('.phone_link', '.popup', '.popup .popup_close');
 	bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
-  bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
-  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+	bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+	bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
 	//showModalByTime('.popup', 6000);
 };
 
